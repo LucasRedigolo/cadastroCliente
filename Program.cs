81a0321a1b5fd3cfa6c9fe0833e29fcb3e9eda69
FIC <afonso.jair@yahoo.com>
2017-10-06 19:40:22 -0300

    Nome-EstadoCivil

--- Program.cs
+++ Program.cs
@@ -6,10 +6,42 @@ namespace cadastroCliente
     {
         static void Main(string[] args)
         {
-            string nome;        
-            Console.WriteLine("Qual o seu nome?");
-            nome = Console.ReadLine();
+            string Nome;
+            string LocalNascimento;
+            string Profissao;
+            string Endereco;
+            int EstadoCivil;
+            string StringEstadoCivil;
+            bool Converteu = false;
 
-        }
+            Console.WriteLine("Qual o seu nome?");
+            Nome = Console.ReadLine();
+
+            Console.WriteLine("Qual o seu local de nascimento?");
+            LocalNascimento = Console.ReadLine();
+
+            Console.WriteLine("Qual a sua profissão?");
+            Profissao = Console.ReadLine();
+
+            Console.WriteLine("Qual o seu endereço?");
+            Endereco = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Qual o seu estado civil?\n1 Solteiro\n2 Casado");
+                Converteu = int.TryParse(Console.ReadLine(), out EstadoCivil);
+
+            } while (Converteu == true && EstadoCivil == 1 || EstadoCivil == 2);
+
+            if(EstadoCivil == 1)
+            {
+                StringEstadoCivil = "Solteiro";
+            }else{
+                StringEstadoCivil = "Casado";
+            }
+
+            Console.WriteLine(StringEstadoCivil);
+
+        }     
     }
 }
