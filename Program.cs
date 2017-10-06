05b6cfa364bc9140777a8dda3fb34618e0fac4ba
FIC <lucas_redigolo@hotmail.com>
2017-10-06 19:07:24 -0300

    inclui idade e rg

--- Program.cs
+++ Program.cs
@@ -6,10 +6,18 @@ namespace cadastroCliente
     {
         static void Main(string[] args)
         {
-            string nome;        
+            string nome;     
+            string RG;   
+            int Idade;
+
             Console.WriteLine("Qual o seu nome?");
             nome = Console.ReadLine();
-
+            Console.WriteLine("Qual o seu RG?");
+            RG = Console.ReadLine();
+            Console.WriteLine("Qual sua idade?");
+            Idade = Convert.ToInt32(Console.ReadLine());
+            
+           
         }
     }
 }
